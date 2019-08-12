3d4b6411627443eac68f0d4cf8891476224800ef
MOHAMMAD ARIZ KHAN <ariz@MOHAMMADs-MacBook-Air.local>
2019-08-12 13:06:43 +0530

    modified

---
+++ stack-using-Array.py
@@ -0,0 +1,33 @@
+class Stack:
+    
+    def __init__(self):
+        self.array = []
+
+# --------------------------------------------------------------------# 
+# creating a Function for pushing element :-  
+      
+    def push(self,value):
+        self.array.append(value)
+        return self.view()
+        
+
+# --------------------------------------------------------------------# 
+# creating a function for viewing :-
+    def view(self):
+        print(self.array)
+# --------------------------------------------------------------------#
+# creating a Function for pushing element :-  
+      
+    def pop(self):
+        self.array.pop()
+        return self.view()
+
+# --------------------------------------------------------------------#    
+    
+
+mystack = Stack()
+mystack.push('google')
+mystack.push('facebook')
+mystack.push('facebook')
+mystack.pop()
+
