bf696e45b1fd6164378643adba3033c1e748d03b
MOHAMMAD ARIZ KHAN <ariz@MOHAMMADs-MacBook-Air.local>
2019-08-12 17:22:43 +0530

    modified

---
+++ queue-using-LinkedList.py
@@ -0,0 +1,103 @@
+# Creating a class Node :-
+class Node:
+
+# Creating a Constructor :-    
+    
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+# --------------------------------------------------------------------#
+
+# Creating a class Stack
+class Stack:
+
+# Creating a Constructor :-
+
+    def __init__(self):
+        self.top = None
+        self.bottom = None
+        
+      
+        
+# --------------------------------------------------------------------#
+
+# creating a Function for pushing element :-  
+      
+    def push(self,value):
+        NewElement = Node(value)
+
+        #previous.next = self.bottom
+        if self.top == None:
+            self.top = NewElement
+            self.bottom = self.top
+            
+        
+        else:
+            temp1 = NewElement 
+            temp2 = self.top
+            self.top = temp1
+            self.top.next = temp2
+            
+       
+
+# --------------------------------------------------------------------# 
+
+#  # creating a Function for deleting from top element :- 
+    
+    def pop(self):
+        if self.top == None:
+            print("stack is empty")
+        elif self.top == self.bottom:
+            self.bottom = None    
+        else: 
+            self.top = self.top.next
+            
+
+# --------------------------------------------------------------------#           
+
+# Creating a function to calculate the length :-
+    
+    def length(self):
+        total = 0
+        temp = self.top
+        if self.top == None:
+            return total
+        else:
+            while True:
+                if temp.next == None:
+                    total = total + 1
+                    break
+                else:
+                    temp = temp.next
+                    total = total + 1        
+            return total        
+  
+
+# --------------------------------------------------------------------#
+
+# Creating a function to view :-
+
+    def view(self):
+        temp = self.top
+        arr = []
+        index = 0
+        while index <= self.length() - 1:
+            arr.append(temp.data)
+            temp = temp.next
+            index = index + 1
+        print(arr)  
+
+# --------------------------------------------------------------------#
+
+mystack = Stack()
+mystack.push('google')
+mystack.push('facebook')
+mystack.push('twitter')
+mystack.push('bbc')
+mystack.view()
+mystack.pop()
+mystack.view()
+
+
+
+
